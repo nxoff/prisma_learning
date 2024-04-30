d186815c02f9912291fe798cd20af1dd3e658513
Narcikss <mdgamedeveloper@gmail.com>
2024-04-30 20:38:37 +0200

    fix vercel deployment

--- app/api/add-post/route.tsx
+++ app/api/add-post/route.tsx
@@ -1,7 +1,7 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextResponse } from "next/server";
 
-export async function POST(req : NextApiRequest, res : NextApiResponse) {
-    if (req.method === 'POST') {
-
-    }
+export async function POST(req : Request) {
+    const res = await req.json()
+    console.log({res})
+    return NextResponse.json({data: res})
 }
